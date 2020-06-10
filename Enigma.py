d9fe0924a9e6e9a4b5e81d218acdf0ada1db4bca
Ashwinjadoe <ashwinjadoe@gmail.com>
2020-06-10 23:45:22 +0200

    Variabelen aangemaakt voor aanroepen Class

--- Enigma.py
+++ Enigma.py
@@ -1,5 +1,11 @@
-class Enigma():
+#https://www.cryptool.org/en/cto-ciphers/enigma
 
+Start1 = 3 
+Start2 = 4
+Start3 = 5
+tekst = "test"
+
+class Enigma():
 
     def __init__(self, Rotset1, Rotset2, Rotset3, Tekst):
         self.Rotset1 = Rotset1
@@ -9,7 +15,8 @@ class Enigma():
         #We kunnen aan het eind dus nog iets er inschrijven dat je de verschillende rotoren kan plaatsen...
         self.Rot1 = "ekmflgdqvzntowyhxuspaibrcj"
         self.Rot2 = "ajdksiruxblhwtmcqgznpyfvoe"
-        self.Rot3 = "fvpjiaoyedrzxwgctkuqsbnmhl"        
+        self.Rot3 = "fvpjiaoyedrzxwgctkuqsbnmhl"       
+        self.Tekst = Tekst 
         
     #functie die de rotoren aan het begin klaarzet, deze kunnen we gebruiken voor recursie dan hoeven we alleen maar de rotorsettings op te hogen!
     def Rotset(self):
@@ -19,10 +26,11 @@ class Enigma():
         Rot3 =self.Rot3[self.Rotset3:26] + self.Rot2[0: self.Rotset3]
         return Rot1, Rot2, Rot3
     
-    def encrypt(self):
+    def Encrypt(self):
+        Enigma().Rotset()
         return
 
-enigma = Enigma(4,2,3, "test")
+enigma = Enigma(Start1, Start2, Start3, tekst)
 
 print(Enigma.Rotset(enigma))
 
