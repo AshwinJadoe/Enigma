50935c414d088a282fd986a86651be106e74e292
Ashwinjadoe <ashwinjadoe@gmail.com>
2020-06-10 13:32:52 +0200

    aanmaak enigma class + __init__

---
+++ Enigma.py
@@ -0,0 +1,8 @@
+class enigma():
+    Rot1 = "abcdefghijklmnopqrstuvwxyz"
+    Rot2 = "abcdefghijklmnopqrstuvwxyz"
+    Rot3 = "abcdefghijklmnopqrstuvwxyz"
+
+    def __init__(self, RotSet, Tekst):
+
+    pass
