6e831e76467c741dff7abda1658bc0bc099114d5
Ashwinjadoe <ashwinjadoe@gmail.com>
2020-06-10 23:31:43 +0200

    comments toegevoegd voor guus en lyor

--- Enigma.py
+++ Enigma.py
@@ -5,19 +5,20 @@ class Enigma():
         self.Rotset1 = Rotset1
         self.Rotset2 = Rotset2
         self.Rotset3 = Rotset3
-       
+        #De originele enigma rotors zaten niet alledrie op alphabetvolgorde maar op deze manier:
+        #We kunnen aan het eind dus nog iets er inschrijven dat je de verschillende rotoren kan plaatsen...
         self.Rot1 = "ekmflgdqvzntowyhxuspaibrcj"
         self.Rot2 = "ajdksiruxblhwtmcqgznpyfvoe"
         self.Rot3 = "fvpjiaoyedrzxwgctkuqsbnmhl"        
         
-
+    #functie die de rotoren aan het begin klaarzet, deze kunnen we gebruiken voor recursie dan hoeven we alleen maar de rotorsettings op te hogen!
     def Rotset(self):
         print(set)
         Rot1 =self.Rot1[self.Rotset1:26] + self.Rot1[0: self.Rotset1]
         Rot2 =self.Rot2[self.Rotset2:26] + self.Rot2[0: self.Rotset2]
         Rot3 =self.Rot3[self.Rotset3:26] + self.Rot2[0: self.Rotset3]
         return Rot1, Rot2, Rot3
-
+    
     def encrypt(self):
         return
 
