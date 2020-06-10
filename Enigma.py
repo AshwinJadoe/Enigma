3b5fad7873c0bb11d5d47044795ade397900f313
Ashwinjadoe <ashwinjadoe@gmail.com>
2020-06-10 23:15:24 +0200

    Input settings veranderd naar individuele input per rotor ipv 1 voor allle 3

--- Enigma.py
+++ Enigma.py
@@ -1,23 +1,26 @@
 class Enigma():
 
 
-    def __init__(self, Rotset):
-        self.Rotset = Rotset
+    def __init__(self, Rotset1, Rotset2, Rotset3, Tekst):
+        self.Rotset1 = Rotset1
+        self.Rotset2 = Rotset2
+        self.Rotset3 = Rotset3
         self.Rot1 = "abcdefghijklmnopqrstuvwxyz"
         self.Rot2 = "abcdefghijklmnopqrstuvwxyz"
         self.Rot3 = "abcdefghijklmnopqrstuvwxyz"        
         
 
-    def EersteRotset(self):
-        set = str(self.Rotset)
+    def Rotset(self):
         print(set)
-        Rot1 =self.Rot1[int(set[0]):26] + self.Rot1[0: int(set[0])]
-        Rot2 =self.Rot2[int(set[1]):26] + self.Rot2[0: int(set[1])]
-        Rot3 =self.Rot3[int(set[2]):26] + self.Rot2[0: int(set[2])]
+        Rot1 =self.Rot1[self.Rotset1:26] + self.Rot1[0: self.Rotset1]
+        Rot2 =self.Rot2[self.Rotset2:26] + self.Rot2[0: self.Rotset2]
+        Rot3 =self.Rot3[self.Rotset3:26] + self.Rot2[0: self.Rotset3]
         return Rot1, Rot2, Rot3
 
+    def encrypt(self):
+        return
 
-enigma = Enigma(423)
+enigma = Enigma(4,2,3, "test")
 
-print(Enigma.EersteRotset(enigma))
+print(Enigma.Rotset(enigma))
 
