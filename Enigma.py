be568ee76333c0b37d0127da368136392ecf16c9
Ashwinjadoe <ashwinjadoe@gmail.com>
2020-06-10 17:05:36 +0200

    Rotorset functie voor de drie rotoren gemaakt

--- Enigma.py
+++ Enigma.py
@@ -8,14 +8,16 @@ class Enigma():
         self.Rot3 = "abcdefghijklmnopqrstuvwxyz"        
         
 
-    def Rotdraaien(self):
-        set= self.Rotset
+    def EersteRotset(self):
+        set = str(self.Rotset)
         print(set)
-        Rot1 =self.Rot1[set:26] + self.Rot1[0: set]
-        return Rot1
+        Rot1 =self.Rot1[int(set[0]):26] + self.Rot1[0: int(set[0])]
+        Rot2 =self.Rot2[int(set[1]):26] + self.Rot2[0: int(set[1])]
+        Rot3 =self.Rot3[int(set[2]):26] + self.Rot2[0: int(set[2])]
+        return Rot1, Rot2, Rot3
 
 
-enigma = Enigma(4)
+enigma = Enigma(423)
 
-print(Enigma.Rotdraaien(enigma))
+print(Enigma.EersteRotset(enigma))
 
