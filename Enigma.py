acff53d43b00063386622dd06b3629bf39f74e45
Ashwinjadoe <ashwinjadoe@gmail.com>
2020-06-15 11:41:13 +0200

    Originele rotorstanden uit de __init__ gehaald

--- Enigma.py
+++ Enigma.py
@@ -6,6 +6,9 @@ Start3 = 5
 tekst = "test"
 
 class Enigma():
+    Rot1 = "ekmflgdqvzntowyhxuspaibrcj"
+    Rot2 = "ajdksiruxblhwtmcqgznpyfvoe"
+    Rot3 = "fvpjiaoyedrzxwgctkuqsbnmhl"  
 
     def __init__(self, Rotset1, Rotset2, Rotset3, Tekst):
         self.Rotset1 = Rotset1
@@ -13,9 +16,7 @@ class Enigma():
         self.Rotset3 = Rotset3
         #De originele enigma rotors zaten niet alledrie op alphabetvolgorde maar op deze manier:
         #We kunnen aan het eind dus nog iets er inschrijven dat je de verschillende rotoren kan plaatsen...
-        self.Rot1 = "ekmflgdqvzntowyhxuspaibrcj"
-        self.Rot2 = "ajdksiruxblhwtmcqgznpyfvoe"
-        self.Rot3 = "fvpjiaoyedrzxwgctkuqsbnmhl"       
+     
         self.Tekst = Tekst 
         
     #functie die de rotoren aan het begin klaarzet, deze kunnen we gebruiken voor recursie dan hoeven we alleen maar de rotorsettings op te hogen!
@@ -27,10 +28,12 @@ class Enigma():
         return Rot1, Rot2, Rot3
     
     def Encrypt(self):
+        
         Enigma().Rotset()
         return
 
 enigma = Enigma(Start1, Start2, Start3, tekst)
 
 print(Enigma.Rotset(enigma))
+
 
